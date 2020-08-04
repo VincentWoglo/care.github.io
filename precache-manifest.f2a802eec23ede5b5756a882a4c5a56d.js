775fd3e612d11b74f9fc3867c62795bbc5eb5f4c
VincentWoglo <vincentwoglo@gmail.com>
2020-08-04 14:38:04 -0600

    Updates

---
+++ precache-manifest.f2a802eec23ede5b5756a882a4c5a56d.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c27d74c7ebf8f8dcf4194c565ee9a848",
+    "url": "/care.github.io/index.html"
+  },
+  {
+    "revision": "bcddf484fb2304747191",
+    "url": "/care.github.io/static/css/main.4b33e22d.chunk.css"
+  },
+  {
+    "revision": "c29c82e1eb973994345c",
+    "url": "/care.github.io/static/js/2.75a159e2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/care.github.io/static/js/2.75a159e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bcddf484fb2304747191",
+    "url": "/care.github.io/static/js/main.17bd5977.chunk.js"
+  },
+  {
+    "revision": "74a7908827c0d9fb4794",
+    "url": "/care.github.io/static/js/runtime-main.7af44f2a.js"
+  },
+  {
+    "revision": "627f5783215503e054a4e5b73008e542",
+    "url": "/care.github.io/static/media/american-express-2.627f5783.svg"
+  },
+  {
+    "revision": "3759e242e120cbaa1241eaf89f6be22c",
+    "url": "/care.github.io/static/media/banner.3759e242.jpg"
+  },
+  {
+    "revision": "d1405c9477be1c386f677f567cf7cf29",
+    "url": "/care.github.io/static/media/creative collection.d1405c94.jpg"
+  },
+  {
+    "revision": "7deb86fcfdbe4724eed7deddd75cb8fa",
+    "url": "/care.github.io/static/media/discount banner.7deb86fc.jpg"
+  },
+  {
+    "revision": "b76f2d7d551d4bff1ff67ec8332e517f",
+    "url": "/care.github.io/static/media/discover-4.b76f2d7d.svg"
+  },
+  {
+    "revision": "e8e6a41e9deddfd36a5c98a6f86eb3bb",
+    "url": "/care.github.io/static/media/iconmonstr-menu-thin.e8e6a41e.svg"
+  },
+  {
+    "revision": "231de5f3e0cba231b5e0ae3316dafbef",
+    "url": "/care.github.io/static/media/iconmonstr-search-thin.231de5f3.svg"
+  },
+  {
+    "revision": "d67ac7c362842678a5b4ec7170c6480a",
+    "url": "/care.github.io/static/media/iconmonstr-shopping-cart-5.d67ac7c3.svg"
+  },
+  {
+    "revision": "edc0f77d88444728f6ec1259ab7b7e41",
+    "url": "/care.github.io/static/media/iconmonstr-user-male-thin.edc0f77d.svg"
+  },
+  {
+    "revision": "ea46f7a798d685204490493adff89a28",
+    "url": "/care.github.io/static/media/mastercard-6.ea46f7a7.svg"
+  },
+  {
+    "revision": "ebc3a88ea568e7a5cba626174eef62bc",
+    "url": "/care.github.io/static/media/paypal-icon.ebc3a88e.svg"
+  },
+  {
+    "revision": "2a1c0a2fd2637e69cff444a38c39591e",
+    "url": "/care.github.io/static/media/special product 1.2a1c0a2f.jpg"
+  },
+  {
+    "revision": "7c555ebba083ec559527735983196382",
+    "url": "/care.github.io/static/media/special product 2.7c555ebb.jpg"
+  },
+  {
+    "revision": "1e23aded8a51692a7b1d1070a4a26bd5",
+    "url": "/care.github.io/static/media/special product 3.1e23aded.jpg"
+  },
+  {
+    "revision": "0e3fb90d2b822538c812a060e6c1c12a",
+    "url": "/care.github.io/static/media/special product 4.0e3fb90d.jpg"
+  },
+  {
+    "revision": "17f37469247dae504a4f423ef315fe2b",
+    "url": "/care.github.io/static/media/special product 5.17f37469.jpg"
+  },
+  {
+    "revision": "d575c9a15a20dd48f6b7be8b43094063",
+    "url": "/care.github.io/static/media/visa-2.d575c9a1.svg"
+  },
+  {
+    "revision": "4ef798adf46f74882c0bbb25db063093",
+    "url": "/care.github.io/static/media/women section.4ef798ad.jpg"
+  }
+]);
